cd8d1e04d8b9eead15b93a3b693741a5eeafdf9b
burak <burak.cbdn@gmail.com>
2020-05-10 21:52:37 +0300

    queue added

--- main.py
+++ main.py
@@ -5,21 +5,38 @@ import googletrans
 import requests
 import youtube_dl
 import discord
+import shutil
 from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
+from os import system
 
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.environ.get('BOT_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-
 bot = commands.Bot(command_prefix="!")
 
 
 async def send_embedded(ctx, content):
     embed = discord.Embed(color=0x00ff00, description=content)
     await ctx.send(embed=embed)
+
+
+def getCovidInfo(country):
+    response = requests.get("https://api.covid19api.com/summary")
+
+    if response.status_code == 200:
+        countries = response.json()["Countries"]
+        country = country
+        for data in countries:
+            if data["Country"].lower() == country.lower():
+                totalConfirmed = data["TotalConfirmed"]
+                totalDeath = data["TotalDeaths"]
+                return {"totalConfirmed": totalConfirmed, "totalDeath": totalDeath}
+
+    else:
+        return
 
 
 @bot.event
@@ -112,21 +129,6 @@ async def display_covid(ctx, country=None):
         await ctx.send(embed=embed)
 
 
-def getCovidInfo(country):
-    response = requests.get("https://api.covid19api.com/summary")
-
-    if response.status_code == 200:
-        countries = response.json()["Countries"]
-        country = country
-        for data in countries:
-            if data["Country"].lower() == country.lower():
-                totalConfirmed = data["TotalConfirmed"]
-                totalDeath = data["TotalDeaths"]
-                return {"totalConfirmed": totalConfirmed, "totalDeath": totalDeath}
-
-    else:
-        return
-
 @bot.command(name="join")
 async def join(ctx):
         # joining the channel
@@ -140,6 +142,7 @@ async def join(ctx):
     else:
         voice = await channel.connect()
         print(f"the bot has connected to {channel} \n")
+
     embed = discord.Embed(color=0x00ff00, description=f"'{channel}' ses kanalına bağlandım")
     await ctx.send(embed=embed)
 
@@ -147,21 +150,70 @@ async def join(ctx):
 @bot.command(name="play")
 async def play(ctx, url: str):
 
+    def check_queue():
+        Queue_infile = os.path.isdir("./Queue")
+        if Queue_infile is True:
+            DIR = os.path.abspath(os.path.realpath("Queue"))
+            length = len(os.listdir(DIR))
+            still_q = length - 1
+            try:
+                first_audio = os.listdir(DIR)[0]
+
+            except:
+                print("queue is empty")
+                queues.clear()
+                return
+            main_path = os.path.dirname(os.path.realpath(__file__)) 
+            q_path = os.path.abspath(os.path.realpath("Queue") + "\\" + first_audio)
+
+            if length != 0:
+                print("playing next song")
+                is_song_exist = os.path.isfile("audio.mp3")
+                if is_song_exist:
+                    os.remove("audio.mp3")
+                shutil.move(q_path, main_path)
+
+                for file in os.listdir("./"):
+                    if file.endswith(".mp3"):
+                        os.rename(file, "audio.mp3")
+
+                voice.play(discord.FFmpegPCMAudio('audio.mp3'), after=lambda e: check_queue())
+                voice.source = discord.PCMVolumeTransformer(voice.source)
+                voice.source.volume = 0.07
+            else: 
+                queues.clear()
+                return
+        else:
+            queues.clear()
+            print("no audio in queue")    
+
 
     # playing audio
-
     is_song_exist = os.path.isfile("audio.mp3")
     try:
         if is_song_exist:
             os.remove("audio.mp3")
+            queues.clear()
             print("old file removed")
 
     except PermissionError:
         embed = discord.Embed(color=0x00ff00, description="Maalesef aynı anda iki ses çalamam.")
         await ctx.send(embed=embed)
         return
+
+    Queue_infile = os.path.isdir("./Queue")
+    try:
+        Queue_folder = "./Queue"
+        if Queue_infile is True:
+            print("old queue folder removed")
+            shutil.rmtree(Queue_folder)
+    except:
+        print("no old queue folder")
+
+
     embed = discord.Embed(color=0x00ff00, description="Sahneye hazırlanıyorum. Beklemelisin :)")
     await ctx.send(embed=embed)
+
     voice = get(bot.voice_clients, guild=ctx.guild)
     ydl_options = {
         'format': 'bestaudio/best',
@@ -182,10 +234,12 @@ async def play(ctx, url: str):
             name = file
             os.rename(file, 'audio.mp3')
 
-    voice.play(discord.FFmpegPCMAudio('audio.mp3'), after=lambda e: print(f"{name} has finished playing"))
+    voice.play(discord.FFmpegPCMAudio('audio.mp3'), after=lambda e: check_queue())
     voice.source = discord.PCMVolumeTransformer(voice.source)
     voice.source.volume = 0.07
+
     name = name.rsplit('-')
+
     embed = discord.Embed(color=0x00ff00, description=f"'{name[0] + name[1]}' çalınıyor")
     await ctx.send(embed=embed)
 
@@ -235,12 +289,55 @@ async def resume(ctx):
 async def stop(ctx):
     voice = get(bot.voice_clients, guild=ctx.guild)
 
+    queues.clear()
+
     if voice and voice.is_playing():
         voice.stop()
         await send_embedded(ctx, "Ses durduruldu")
 
     else:
         await send_embedded(ctx, "Ortalık zaten sessiz.")
+
+
+queues = {}
+
+@bot.command(name="queue")
+async def queue(ctx, url: str):
+    Queue_infile = os.path.isdir("./Queue")
+    if Queue_infile is False:
+        os.mkdir("Queue")
+
+    DIR = os.path.abspath(os.path.realpath("Queue"))
+    q_num = len(os.listdir(DIR))
+    q_num += 1
+    add_queue = True 
+    while add_queue:
+        if q_num in queues:
+            q_num += 1
+        else:
+            add_queue = False
+            queues[q_num] = q_num
+
+    queue_path = os.path.abspath(os.path.realpath("Queue") + f"\\audio{q_num}.%(ext)s")
+
+    ydl_options = {
+    'format': 'bestaudio/best',
+    'outtmpl':queue_path,
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192'
+
+        }]
+    }
+
+    with youtube_dl.YoutubeDL(ydl_options) as ydl:
+        print("downloading audio")
+        ydl.download([url])
+
+    await send_embedded(ctx, f"ses{q_num} sıraya eklendi!")    
+
+    print("sond added to queue")
 
 
 bot.run(TOKEN)
